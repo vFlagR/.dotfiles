2e149f5f682b0433bc499f84fbfae79c2e617e37
Kieran Mceniff <info@kieranmceniff.com>
2019-09-05 18:07:57 +0100

    Adding more dotfiles

---
+++ zsh/alias.zsh
@@ -0,0 +1,13 @@
+# Edit files alias'
+alias ezsh="vim ~/.dotfiles/zsh/zshrc"
+
+# Source file alias'
+alias szsh="source ~/.dotfiles/zsh/zshrc"
+
+# Cd Alias'
+alias cdot="cd ~/.dotfiles"
+
+# Colorls Alias'
+alias ls="colorls"
+alias "ls -al"="colorls -al"
+alias ll="colorls -al"
---
+++ zsh/env.zsh
@@ -0,0 +1,1 @@
+# Environemnt variables store
