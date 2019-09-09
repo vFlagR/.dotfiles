53582d9e711531d12e55a0212de88d6eec59bf56
Kieran Mceniff <info@kieranmceniff.com>
2019-09-09 11:30:02 +0100

    working on git config

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,13 +1,20 @@
 # Edit files alias'
-alias ezsh="vim ~/.dotfiles/zsh/zshrc"
+alias ezsh="vim ~/.dotfiles/zsh/.zshrc"
+alias ealias="vim ~/.dotfiles/zsh/alias.zsh"
+alias eplugins="vim ~/.dotfiles/zsh/plugins.txt"
 
 # Source file alias'
-alias szsh="source ~/.dotfiles/zsh/zshrc"
+alias szsh="source ~/.dotfiles/zsh/.zshrc"
 
 # Cd Alias'
 alias cdot="cd ~/.dotfiles"
+alias cdata="cd ~/data"
+alias cpg="cd ~/data/personal/pgblitz"
 
 # Colorls Alias'
 alias ls="colorls"
 alias "ls -al"="colorls -al"
 alias ll="colorls -al"
+
+# Ssh Alias'
+alias ssg="ssh golang"
